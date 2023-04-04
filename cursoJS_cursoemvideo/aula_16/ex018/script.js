e4338e9051b7377b66960cd36cac3cec886da799
Luiz Silva <luizgpereiras22@gmail.com>
2023-04-04 17:53:01 -0300

    study hours counter

---
+++ cursoJS_cursoemvideo/aula_16/ex018/script.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    var num = Number(window.document.getElementById('inum'))
+    var n
+    if (num >= 1 && num <= 100){
+        //adiciona a lista
+        n.push(num)
+    } else {
+        alert('Valor Inválido!')
+    }
+}
+
+function analisar(){
+
+}
---
+++ exercicios/calculadora/script.js
@@ -0,0 +1,11 @@
+function calcular (){
+    var x1 = document.getElementById('x1')
+    var x2 = ducument.getElementById('x2')
+    var res = ducument.getElementById('resultado')
+
+    if (x1.value.length == 0 || x2.value.length == 0){
+        alert('Dados Inválidos')
+    } else{
+        alert('Ok')
+    }
+}
